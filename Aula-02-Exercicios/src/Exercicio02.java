1eeab9596e150a4829db50895bc208e6f8f19b19
Alécio <alecio.godoi@ymail.com>
2021-02-19 22:49:48 -0300

    Aula 02 - Exercícios

--- Aula-02-Exercicios/src/Exercicio02.java
+++ Aula-02-Exercicios/src/Exercicio02.java
@@ -7,10 +7,11 @@ public class Exercicio02 {
 		// calcule e mostre a média aritmética entre elas.
 		
 		float n1, n2, n3, media;
+		
 		n1 = Float.valueOf(JOptionPane.showInputDialog("Numero 1:"));
 		n2 = Float.valueOf(JOptionPane.showInputDialog("Numero 2:"));
 		n3 = Float.valueOf(JOptionPane.showInputDialog("Numero 3:"));
-		media = (n1+n2+n3)/3;
+		media = (n1+n2+n3);
 		
 		JOptionPane.showMessageDialog(null, "Média: " + String.format("%.2f", media ));
 
