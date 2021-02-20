670f80eecf9236bf8a9020c38940f8031da25500
Alécio <alecio.godoi@ymail.com>
2021-02-19 22:00:56 -0300

    Aula 02 - Exercícios

--- EntradaDados/src/EntradaDados.java
+++ EntradaDados/src/EntradaDados.java
@@ -7,19 +7,15 @@ public class EntradaDados {
 		String nome;
 		int idade;
 		/*
-	    Scanner meuScanner = new Scanner(System.in); // Cria um objeto Scanner
-		
+	    Scanner meuScanner = new Scanner(System.in); // Cria um objeto Scanner	
 	    System.out.println("Entre com seu nome: ");
 	    nome  = meuScanner.nextLine();               // Lê os dados digitados
-	    
 	    System.out.println("Entre com sua idade: ");
 	    idade  = meuScanner.nextInt();               // Lê os dados digitados
-	    
 	    System.out.println("Olá "+nome+"\nVocê tem "+idade+" ano(s)");
 	    */
-	    nome = JOptionPane.showInputDialog("Entre com seu nome: ");
+	    nome = JOptionPane.showInputDialog("Entre com seu nome: "); 
 	    idade = Integer.valueOf(JOptionPane.showInputDialog("Entre com sua idade: "));
-	    JOptionPane.showMessageDialog(null, "Olá "+nome+"!!!\nSua idade é: "+idade+" ano(s)");	    
-	    
+	    JOptionPane.showMessageDialog(null, "Olá "+nome+"!!!\nSua idade é: "+idade+" ano(s)");	     
 	}
 }
