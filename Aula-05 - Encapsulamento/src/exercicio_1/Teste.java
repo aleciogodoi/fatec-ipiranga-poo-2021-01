60bd9af7937bca6245dcebf10edfe55a3432dce9
Alécio <alecio.godoi@ymail.com>
2021-03-12 22:01:53 -0300

    Aula 05 - Exercicio 1

---
+++ Aula-05 - Encapsulamento/src/exercicio_1/Teste.java
@@ -0,0 +1,13 @@
+package exercicio_1;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		Aluno pedro = new Aluno("Pedro da Silva", "12134-X", 5, 5, 7);
+		System.out.println(pedro);
+
+		Aluno maria = new Aluno("Maria dos Santos", "9801-X", 4, 7, 7);
+		System.out.println(maria);
+	}
+
+}
