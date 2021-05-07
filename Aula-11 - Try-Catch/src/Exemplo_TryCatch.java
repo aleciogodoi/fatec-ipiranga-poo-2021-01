cc1f1e14b6c5513dee3bc1bb37bff724a3d3ae97
Alécio <alecio.godoi@ymail.com>
2021-05-07 20:19:47 -0300

    Aula 11 - Exemplo Try-Catch

---
+++ Aula-11 - Try-Catch/src/Exemplo_TryCatch.java
@@ -0,0 +1,38 @@
+import javax.swing.JOptionPane;
+
+public class Exemplo_TryCatch {
+
+	static int n1, n2; 
+	static boolean flagEx;
+	public static void main(String[] args) {
+		do {
+			flagEx = false;
+			entradaDados();
+			divide();
+		} while(flagEx);
+		System.out.println("FIM!!!");
+	}
+	
+	public static void entradaDados() {
+		try {
+			n1 = Integer.valueOf(JOptionPane.showInputDialog("Entre com n1:"));
+			n2 = Integer.valueOf(JOptionPane.showInputDialog("Entre com n2:"));
+		} catch(NumberFormatException ex) {
+			flagEx = true;
+			JOptionPane.showMessageDialog(null,"n1 e n2 devem ser INTEIROS!!!");
+		}
+	}
+	
+	public static void divide() {
+		try {
+			JOptionPane.showMessageDialog(null, n1+"/"+n2+" = "+(n1/n2));
+			JOptionPane.showMessageDialog(null,"divide() - concluído!");
+		} catch (ArithmeticException ex) {
+			JOptionPane.showMessageDialog(null,"n2 não pode ser ZERO!!!");
+			flagEx = true;
+		} finally {
+			JOptionPane.showMessageDialog(null,"divide() - FIM!");
+		}
+	}
+
+}
