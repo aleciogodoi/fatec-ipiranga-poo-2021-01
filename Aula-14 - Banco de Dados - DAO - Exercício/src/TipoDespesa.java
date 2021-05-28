381eeaf131b6663431149d3e66dc4f94c895f61d
Alécio <alecio.godoi@ymail.com>
2021-05-28 19:54:07 -0300

    Aula-14- Banco de Dados DAO - Exercício

---
+++ Aula-14 - Banco de Dados - DAO - Exercício/src/TipoDespesa.java
@@ -0,0 +1,34 @@
+public class TipoDespesa {
+	// Atributos
+	private int idTipo;
+	private String descrTipo;
+	
+	// Construtores
+	public TipoDespesa() {
+	}
+
+	public TipoDespesa(String descrTipo) {
+		this.descrTipo = descrTipo;
+	}
+
+	public int getIdTipo() {
+		return idTipo;
+	}
+
+	public void setIdTipo(int idTipo) {
+		this.idTipo = idTipo;
+	}
+
+	public String getDescrTipo() {
+		return descrTipo;
+	}
+
+	public void setDescrTipo(String descrTipo) {
+		this.descrTipo = descrTipo;
+	}
+
+	@Override
+	public String toString() {
+		return "TipoDespesa [idTipo=" + idTipo + ", descrTipo=" + descrTipo + "]";
+	} 
+}
