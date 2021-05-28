6c74f6edaf80acf69d9dfbe4198864fb5490d104
Alécio <alecio.godoi@ymail.com>
2021-05-28 20:23:12 -0300

    Aula-14- Banco de Dados DAO - Exercício

--- Aula-14 - Banco de Dados - DAO - Exercício/src/Teste.java
+++ Aula-14 - Banco de Dados - DAO - Exercício/src/Teste.java
@@ -1,11 +1,20 @@
 import java.sql.Connection;
 
+import javax.swing.JOptionPane;
+
 public class Teste {
 
 	public static void main(String[] args) {
 		Connection conn;
 		Conexao conexao = new Conexao();
 		conn = conexao.abrir();
+		
+		// Testar find
+		JOptionPane.showMessageDialog(null, TipoDespesaDAO.find(1));
+		
+		// Testar getAll
+		JOptionPane.showMessageDialog(null, TipoDespesaDAO.getAll());
+		
 		conexao.fechar(conn);
 	}
 
