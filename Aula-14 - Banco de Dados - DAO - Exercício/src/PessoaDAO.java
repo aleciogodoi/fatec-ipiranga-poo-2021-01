04b52b36ec788dc005caa3cc1a63e19f9aa946a3
Alécio <alecio.godoi@ymail.com>
2021-06-04 19:32:05 -0300

    Aula 15 - Banco de Dados - DAO - Exercício

---
+++ Aula-14 - Banco de Dados - DAO - Exercício/src/PessoaDAO.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class PessoaDAO {
+	
+	public static void insert(Pessoa pessoa) {	
+	}
+	
+	public static void update(Pessoa pessoa) {
+	}
+	
+	public static void delete(int idPessoa) {
+	}
+	
+	public static Pessoa find(int idPessoa) {
+		Pessoa pessoa = new Pessoa();
+		return pessoa;
+	}
+	
+	public static ArrayList<Pessoa> getAll() {
+		ArrayList<Pessoa> pessoas = new ArrayList<Pessoa>();
+		return pessoas;
+	}
+
+}
