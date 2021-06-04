57e69a5e45d8bc702f53161e59596ce4b1b94d65
Alécio <alecio.godoi@ymail.com>
2021-06-04 20:34:28 -0300

    Aula 15 - Banco de Dados - DAO - Exercício

--- Aula-14 - Banco de Dados - DAO - Exercício/src/PessoaDAO.java
+++ Aula-14 - Banco de Dados - DAO - Exercício/src/PessoaDAO.java
@@ -1,23 +1,184 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class PessoaDAO {
 	
 	public static void insert(Pessoa pessoa) {	
+		// String com comando SQL
+		String sql = "Insert Into Pessoa (cpf, dataNascimento, nome, email, peso) Values (?, ?, ?, ?, ?)";
+
+		// Criação de objeto para execução de comandos SQL
+		PreparedStatement comandoSQL;
+		
+		// Objeto para conexão com o BD
+		Connection conn;
+		// Criação de uma conexão com o BD
+		Conexao conexao = new Conexao();
+		conn = conexao.abrir();
+
+		try {
+			// Execução de instruções SQL
+			comandoSQL = conn.prepareStatement(sql);
+		
+			// Set Parametro e executa
+			comandoSQL.setString(1, pessoa.getCpf());
+			comandoSQL.setDate(2, pessoa.getDataNascimento());
+			comandoSQL.setString(3, pessoa.getNome());
+			comandoSQL.setString(4, pessoa.getEmail());
+			comandoSQL.setFloat(5, pessoa.getPeso());
+			comandoSQL.executeUpdate();
+			
+			System.out.println();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		conexao.fechar(conn);
 	}
 	
 	public static void update(Pessoa pessoa) {
+		// String com comando SQL
+		String sql = "Update Pessoa Set cpf=?, dataNascimento=?, nome=?, email=?, peso=? Where idPessoa=?";
+
+		// Criação de objeto para execução de comandos SQL
+		PreparedStatement comandoSQL;
+		
+		// Objeto para conexão com o BD
+		Connection conn;
+		// Criação de uma conexão com o BD
+		Conexao conexao = new Conexao();
+		conn = conexao.abrir();
+
+		try {
+			// Execução de instruções SQL
+			comandoSQL = conn.prepareStatement(sql);
+		
+			// Set Parametro e executa
+			comandoSQL.setString(1, pessoa.getCpf());
+			comandoSQL.setDate(2, pessoa.getDataNascimento());
+			comandoSQL.setString(3, pessoa.getNome());
+			comandoSQL.setString(4, pessoa.getEmail());
+			comandoSQL.setFloat(5, pessoa.getPeso());
+			comandoSQL.setInt(6, pessoa.getIdPessoa());
+			comandoSQL.executeUpdate();
+			
+			System.out.println();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		conexao.fechar(conn);
 	}
 	
 	public static void delete(int idPessoa) {
+		// String com comando SQL
+		String sql = "Delete From Pessoa Where idPessoa = ?";
+
+		// Criação de objeto para execução de comandos SQL
+		PreparedStatement comandoSQL;
+		
+		// Objeto para conexão com o BD
+		Connection conn;
+		// Criação de uma conexão com o BD
+		Conexao conexao = new Conexao();
+		conn = conexao.abrir();
+
+		try {
+			// Execução de instruções SQL
+			comandoSQL = conn.prepareStatement(sql);
+		
+			// Set Parametro e executa
+			comandoSQL.setInt(1, idPessoa);
+			comandoSQL.executeUpdate();
+			
+			System.out.println();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		conexao.fechar(conn);
 	}
 	
 	public static Pessoa find(int idPessoa) {
 		Pessoa pessoa = new Pessoa();
+		// String com comando SQL
+		String sql = "Select * From Pessoa Where idPessoa = ?";
+
+		// Criação de objeto para execução de comandos SQL
+		PreparedStatement comandoSQL;
+		// Criação de um objeto para resultado de Select's
+		ResultSet rs;
+
+		// Objeto para conexão com o BD
+		Connection conn;
+		// Criação de uma conexão com o BD
+		Conexao conexao = new Conexao();
+		conn = conexao.abrir();
+
+		try {
+			// Execução de instruções SQL
+			comandoSQL = conn.prepareStatement(sql);
+		
+			// Set Parametro e executa
+			comandoSQL.setInt(1, idPessoa);
+			rs = comandoSQL.executeQuery();
+			
+			if (rs.next()) { // lê uma linha do ResultSet
+				pessoa.setIdPessoa(rs.getInt("idPessoa"));
+				pessoa.setCpf(rs.getString("cpf"));
+				pessoa.setDataNascimento(rs.getDate("dataNascimento"));
+				pessoa.setNome(rs.getString("nome"));
+				pessoa.setEmail(rs.getString("email"));
+				pessoa.setPeso(rs.getFloat("peso"));
+			}
+			
+			System.out.println();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		conexao.fechar(conn);
 		return pessoa;
 	}
 	
 	public static ArrayList<Pessoa> getAll() {
 		ArrayList<Pessoa> pessoas = new ArrayList<Pessoa>();
+		// String com comando SQL
+		String sql = "Select * From Pessoa";
+
+		// Criação de objeto para execução de comandos SQL
+		PreparedStatement comandoSQL;
+		// Criação de um objeto para resultado de Select's
+		ResultSet rs;
+
+		// Objeto para conexão com o BD
+		Connection conn;
+		// Criação de uma conexão com o BD
+		Conexao conexao = new Conexao();
+		conn = conexao.abrir();
+
+		try {
+			// Execução de instruções SQL
+			comandoSQL = conn.prepareStatement(sql);
+		
+			// Set Parametro e executa
+			rs = comandoSQL.executeQuery();
+			
+			while (rs.next()) { // lê uma linha do ResultSet
+				Pessoa pessoa = new Pessoa();
+				pessoa.setIdPessoa(rs.getInt("idPessoa"));
+				pessoa.setCpf(rs.getString("cpf"));
+				pessoa.setDataNascimento(rs.getDate("dataNascimento"));
+				pessoa.setNome(rs.getString("nome"));
+				pessoa.setEmail(rs.getString("email"));
+				pessoa.setPeso(rs.getFloat("peso"));
+				pessoas.add(pessoa);
+			}
+			
+			System.out.println();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		conexao.fechar(conn);
 		return pessoas;
 	}
 
--- Aula-14 - Banco de Dados - DAO - Exercício/src/Teste.java
+++ Aula-14 - Banco de Dados - DAO - Exercício/src/Teste.java
@@ -1,4 +1,6 @@
 import java.sql.Connection;
+import java.sql.Date;
+import java.time.LocalDate;
 
 import javax.swing.JOptionPane;
 
@@ -6,6 +8,7 @@ public class Teste {
 
 	public static void main(String[] args) {
 		
+		/*
 		// Teste Insert
 		TipoDespesaDAO.insert(new TipoDespesa("Contas de Consumo"));
 		
@@ -20,7 +23,18 @@ public class Teste {
 		
 		// Teste delete
 		TipoDespesaDAO.delete(1);
+		*/
 
+		//PessoaDAO.insert(new Pessoa("1223323-32",Date.valueOf(LocalDate.of(2000, 3, 10)),"Pedro", "pedro@gmail.com", 72.71f ));
+		//PessoaDAO.insert(new Pessoa("2345762-21",Date.valueOf(LocalDate.of(1998, 5, 23)),"Maria", "maria@gmail.com", 55.48f ));
+		//PessoaDAO.update(new Pessoa(5,"2345762-21",Date.valueOf(LocalDate.of(1998, 7, 23)),"Maria Aparecida", "maria@gmail.com", 54.48f ));
+		//PessoaDAO.delete(1);
+		//PessoaDAO.delete(2);
+
+		System.out.println(PessoaDAO.find(4));
+		System.out.println(PessoaDAO.find(5));
+		for (Pessoa pessoa: PessoaDAO.getAll()) 
+			System.out.println(pessoa);
 		
 	}
 
